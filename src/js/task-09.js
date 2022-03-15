da1f6fdd2578effe250db548167f4dbbee89b589
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-03-15 22:14:06 +0200

    task-7,8,9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const colorEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', onButtonChangeColor);
+
+function onButtonChangeColor(event) {
+  const changingColor = getRandomHexColor()
+  bodyEl.style.backgroundColor = changingColor;
+  colorEl.textContent = changingColor;
+  
+}
