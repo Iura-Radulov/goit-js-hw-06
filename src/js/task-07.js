8f86f09f1a40b87cdd76a5ef440fd3d03399d1e3
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-03-16 15:18:27 +0200

    Deleted files

--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const inputEl = document.querySelector('#font-size-control');
-const textEl = document.querySelector('#text');
-
-inputEl.addEventListener('input', changeTextSize);
-
-function changeTextSize(event) {    
-    textEl.style.fontSize = `${event.currentTarget.value}px`;
-}
